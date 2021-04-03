08668f6650d5ef98b6dc97a37591b8f29239dd69
sachinjachak2001 <sachinjachak10@gmail.com>
2021-04-03 09:45:15 +0530

    Updates

---
+++ precache-manifest.c69053929b5c6dd2a3b60891d3710254.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "860e74287e8b5eee624e",
+    "url": "/react-lights-game/static/css/main.ac33fc42.chunk.css"
+  },
+  {
+    "revision": "860e74287e8b5eee624e",
+    "url": "/react-lights-game/static/js/main.860e7428.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/react-lights-game/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "23b930621fd28b94a095",
+    "url": "/react-lights-game/static/js/runtime~main.23b93062.js"
+  },
+  {
+    "revision": "31e3f6aa296e534d5acae8d4c8d62006",
+    "url": "/react-lights-game/index.html"
+  }
+];
